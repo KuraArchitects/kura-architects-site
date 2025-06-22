f3b4856564257748f8129c9d40039dd5bad660f6
Rav <hello@kura-architects.co.uk>
2025-06-22 21:39:15 +0100

    Page.js removed coding error

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <div className="min-h-screen flex flex-col bg-white text-gray-800 font-sans">
 
-      {{/* Main Content */}}
+      {/* Main Content */}
       <section className="flex-grow flex items-center justify-center text-center px-6 py-16">
         <h1 className="text-2xl md:text-3xl font-semibold">
           This section is currently being updated. Please check back soon or <Link href="/contact" className="underline text-[#7994a0]">get in touch</Link> for more information.
